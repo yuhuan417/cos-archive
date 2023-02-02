7840f83b611dc906643f3052568e236095b94b3c
Huan Yu <huan.yu@gmail.com>
2023-02-02 10:43:04 +0800

    No snapshot

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"flag"
 	"log"
-	"os"
 	"path"
-	"time"
 
 	"github.com/allan-simon/go-singleinstance"
 )
@@ -38,31 +36,14 @@ func main() {
 	} else if *action == "link" {
 		log.Println("Action: link")
 		linkFiles(config)
-	} else {
-		os.MkdirAll(path.Join(config.WorkingDir, "backup"), os.ModePerm)
-
-		// /share/CACHEDEV1_DATA/Public/@Recently-Snapshot/GMT+08_2022-03-11_0100
-		now := time.Now()
-		targetPath := "/share/CACHEDEV1_DATA/Public/@Recently-Snapshot/GMT+08_" + now.Format("2006-01-02") + "_0100"
-		if _, err := os.Lstat(targetPath); err != nil {
-			log.Println("Target doesn't exist: ", err)
-		} else {
-			symlinkPath := path.Join(config.WorkingDir, "/Recently-Snapshot")
-			if _, err := os.Lstat(symlinkPath); err == nil {
-				os.Remove(symlinkPath)
-			}
-			if err := os.Symlink(targetPath, symlinkPath); err == nil {
-				if *action == "backup" {
-					log.Println("Action: backup")
-					backupFiles(config)
-				} else if *action == "fsck" {
-					log.Println("Action: fsck")
-					fsckRemote(config)
-				} else if *action == "verify" {
-					log.Println("Action: verify")
-					verifyFiles(config)
-				}
-			}
-		}
+	} else if *action == "backup" {
+		log.Println("Action: backup")
+		backupFiles(config)
+	} else if *action == "fsck" {
+		log.Println("Action: fsck")
+		fsckRemote(config)
+	} else if *action == "verify" {
+		log.Println("Action: verify")
+		verifyFiles(config)
 	}
 }
