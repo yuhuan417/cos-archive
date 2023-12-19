9dde03aec5a1fcddf05ab48142aa12499de0c16e
Huan Yu <huan.yu@gmail.com>
2023-12-19 14:54:30 +0800

    fix

--- backup.go
+++ backup.go
@@ -37,7 +37,7 @@ func uploadPayload(c *COS, config Config, ctx UploadCTX) {
 }
 
 func uploadFiles(config Config, localIndex *Index, remoteIndex *Index) {
-	deleteTime := time.Now().AddDate(0, 1, 0).Unix()
+	deleteTime := time.Now().AddDate(0, 6, 0).Unix()
 	remoteChunkMap := buildChunksMap(*remoteIndex)
 	remoteIndex.Entries.Files = nil
 	remoteIndex.Entries.Dirs = nil
